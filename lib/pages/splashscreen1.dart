86082c27ccb564b851f994b887bd1f3244c96b7b
helenoktaa <1123150205@global.ac.id>
2025-12-13 12:51:59 +0700

    docs: add fonts

--- lib/pages/splashscreen1.dart
+++ lib/pages/splashscreen1.dart
@@ -6,25 +6,33 @@ class SplashScreenPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-            body: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(height: 50),
-                  Container(
-                    width: 200,
-                    height: 200,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.brown,
-                      
-                    ),
-                  )
-
-                ],
-                
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 50),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber[50],
+                image: DecorationImage(
+                  image: AssetImage("assets/images/image1.png"),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          );
+
+            SizedBox(height: 10),
+            Text(
+              "Keep your notes and\nideas in one simple space.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
