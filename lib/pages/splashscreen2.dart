3b9fd325587fc836e2b7281dcafc15bba99fa791
Rian-61 <rian80344@gmail.com>
2025-12-21 21:50:47 +0700

    feat: Membuat image layout di SplashScreen2

--- lib/pages/splashscreen2.dart
+++ lib/pages/splashscreen2.dart
@@ -1,2 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:project_mobile/pages/splashscreen3.dart';
+
+class SplashScreen2 extends StatelessWidget {
+  const SplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber[50],
+                image: DecorationImage(
+                  image: AssetImage("assets/images/imagee.png"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
