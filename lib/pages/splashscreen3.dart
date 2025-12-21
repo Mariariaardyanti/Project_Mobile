f47bacd5bca10fc0c353e33bce3d394a5bea4c6d
Mariariaardyanti <riaardyanti172gmail.com>
2025-12-21 22:28:50 +0700

    Finalize splash screens before switching branch

---
+++ lib/pages/splashscreen3.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:project_mobile/pages/splashscreen4.dart';
+
+class SplashScreen3 extends StatelessWidget {
+  const SplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 30),
+            Container(
+              width: 200,
+              height: 200,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.amber[50],
+                image: DecorationImage(
+                  image: AssetImage("assets/images/imageee.png"),
+                ),
+              ),
+            ),
+
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFedede9),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFFedede9),
+                  ),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color(0xFF745624),
+                  ),
+                ),
+              ],
+            ),
+
+            SizedBox(height: 20),
+            Text(
+              "Shape your workspace\nto fit your style.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.brown[800],
+              ),
+            ),
+
+            SizedBox(height: 10),
+            Text(
+              "Use blocks, templates, and layouts\nthat work the way you do.",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 12.0, color: Colors.brown),
+            ),
+
+            SizedBox(height: 40),
+            Container(
+              margin: const EdgeInsets.only(left: 40, right: 40),
+              child: SizedBox(
+                height: 44,
+                width: double.infinity,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SplashScreen4()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFF745624),
+                  ),
+                  child: const Text(
+                    'Let’s get started',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
