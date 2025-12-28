32ad36594082b46389da44bde06e4307015c0e37
AriPurwoAji <ajiaripurwo@gmail.com>
2025-12-28 16:28:15 +0700

    Membuat bottom navigation bar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -5,6 +5,24 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Dashboard"), actions: const []),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: const Column(children: []),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_outlined), label: ''),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: ''),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.workspace_premium_outlined),
+            label: '',
+          ),
+        ],
+      ),
+    );
   }
 }
