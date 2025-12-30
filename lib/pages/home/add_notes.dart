fa5e2f4f60b35e56579cf7eefb8d55af07ed73c0
Mariariaardyanti <riaardyanti172gmail.com>
2025-12-30 23:42:37 +0700

    feat: add app bar title and back button

--- lib/pages/home/add_notes.dart
+++ lib/pages/home/add_notes.dart
@@ -6,8 +6,17 @@ class AddNotesPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  backgroundColor: const Color(0xFFF9F5EF),
-  body: Container(),
-    );
+    backgroundColor: const Color(0xFFF9F5EF),
+    body: Container (),
+    appBar: AppBar(
+      backgroundColor: Colors.transparent,
+    elevation: 0,
+    leading: const Icon(Icons.arrow_back, color: Colors.black),
+    title: const Text(
+      "Add notes",
+      style: TextStyle(color: Colors.black),
+       ),
+      ),
+    );  
   }
 }
