b9330eaf6a7532bd6b6c78edab190fa58299f836
Mariariaardyanti <riaardyanti172gmail.com>
2025-12-30 23:39:20 +0700

    feat: add scaffold structure for add notes page

---
+++ lib/pages/home/add_notes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddNotesPage extends StatelessWidget {
+  const AddNotesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
