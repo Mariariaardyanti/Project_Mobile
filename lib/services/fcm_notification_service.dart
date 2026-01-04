79ea42d00a68e0b191359e4f156da03cebe4f4cb
Rian-61 <rian80344@gmail.com>
2026-01-04 20:54:31 +0700

    feat: request notification permission in FCM service

--- lib/services/fcm_notification_service.dart
+++ lib/services/fcm_notification_service.dart
@@ -6,5 +6,17 @@ class FCMNotificationService {
 
   Future<void> init({
     required Function(String message) onMessageReceived,
-  }) async {}
+  }) async {
+
+    NotificationSettings settings =
+        await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (kDebugMode) {
+      print('🔔 Notification permission: ${settings.authorizationStatus}');
+    }
+  }
 }
