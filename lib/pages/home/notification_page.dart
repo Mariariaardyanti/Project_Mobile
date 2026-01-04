758f4be3fbeb7ca7bac66cf2aecc8cfe701a4f36
Rian-61 <rian80344@gmail.com>
2026-01-04 23:55:03 +0700

    chore: membuat rangka notificationpage

---
+++ lib/pages/home/notification_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NotificationPage extends StatelessWidget {
+  final List<String> notifications;
+
+  const NotificationPage({
+    super.key,
+    required this.notifications,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox();
+  }
+}
