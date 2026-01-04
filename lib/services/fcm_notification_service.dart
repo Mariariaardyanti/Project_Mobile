815799fcb38f6b82b0e3afdada6cd3a96b2029d6
Rian-61 <rian80344@gmail.com>
2026-01-04 20:43:29 +0700

    chore: menambahkan import flutter foundation untuk debug logging

--- lib/services/fcm_notification_service.dart
+++ lib/services/fcm_notification_service.dart
@@ -1,5 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
-
+import 'package:flutter/foundation.dart';
 
 class FCMNotificationService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
