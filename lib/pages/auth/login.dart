10822e873b2c8d8e828809ae664e79be70fd13ae
helenoktaa <1123150205@global.ac.id>
2026-01-05 11:28:06 +0700

    fix: remove redundant firebase_core import

--- lib/pages/auth/login.dart
+++ lib/pages/auth/login.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:project_mobile/pages/home/homepage.dart';
 import 'package:project_mobile/pages/auth/signup.dart';
