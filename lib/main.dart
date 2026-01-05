a845eed71e778daef3b69c60dd1170ab0f18059a
helenoktaa <1123150205@global.ac.id>
2026-01-05 11:08:42 +0700

    delete import homepage in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'pages/onboarding/splashscreenboard.dart';
-import 'pages/home/homepage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
