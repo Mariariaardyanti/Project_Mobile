50908889c9bbeb276eb78a08a953fef745cc8bfc
helenoktaa <1123150205@global.ac.id>
2026-01-07 07:16:25 +0700

    add class note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,5 +1,28 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Note {
+   final String id;
+  final String title;
+  final String content;
+  final List<String> labels;
+  final List<String> imageUrls;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final String userId;
+  final bool isPinned;
+  final bool isArchived;
   
+  Note({
+    required this.id,
+    required this.title,
+    required this.content,
+    this.labels = const [],
+    this.imageUrls = const [],
+    this.createdAt,
+    this.updatedAt,
+    required this.userId,
+    this.isPinned = false,
+    this.isArchived = false,
+  });
 }
+
