ddcb207929935fbd965c5b85633da008f2c71944
helenoktaa <1123150205@global.ac.id>
2026-01-07 07:34:26 +0700

    feat(notes): add Firestore service to create notes

--- lib/services/notes_service.dart
+++ lib/services/notes_service.dart
@@ -1,3 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:project_mobile/models/note_model.dart';
 
+class NotesService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final String collection = 'notes';
+
+  //create notes
+  Future<void> addNote(Note note) async {
+    try {
+      await _firestore.collection(collection).add(note.toMap());
+    } catch (e) {
+      print('Error adding note: $e');
+      rethrow;
+    }
+  }
+
+}
