ea791d34f481553ebdcb55d2d85c59346a9de263
helenoktaa <1123150205@global.ac.id>
2026-01-12 15:47:47 +0700

    update(profile): add import package edit-profile.dart

--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -7,6 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:project_mobile/pages/profile/edit_profile.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
