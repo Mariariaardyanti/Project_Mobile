45ec2cf8b8aa917f1affeebdb693a3e4b74092d8
helenoktaa <1123150205@global.ac.id>
2026-01-12 15:12:07 +0700

    feat: add stateful widget

--- lib/pages/profile/edit_profile.dart
+++ lib/pages/profile/edit_profile.dart
@@ -1,3 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as fb;
 import 'package:cloud_firestore/cloud_firestore.dart';
+
+class EditProfile extends StatefulWidget {
+  final Map<String, dynamic> userData;
+
+  const EditProfile({super.key, required this.userData});
+
+  @override
+  State<EditProfile> createState() => _EditProfileState();
+}
+
