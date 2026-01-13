51e27f7f22e275cb1b49d089764189efa366de76
Rian-61 <rian80344@gmail.com>
2026-01-13 21:00:51 +0700

    feat: add firebase messaging background handler function

--- lib/services/fcm_background_handler.dart
+++ lib/services/fcm_background_handler.dart
@@ -1,3 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import '../firebase_options.dart';
+
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  
+}
