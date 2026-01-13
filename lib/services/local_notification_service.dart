cd1dab9b56756ac0a89ed59c347666f77bd0c6ea
Rian-61 <rian80344@gmail.com>
2026-01-13 21:23:25 +0700

    chore: add flutter local notifications plugin instance

--- lib/services/local_notification_service.dart
+++ lib/services/local_notification_service.dart
@@ -2,4 +2,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 
 class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _plugin =
+    FlutterLocalNotificationsPlugin();
+
 }
