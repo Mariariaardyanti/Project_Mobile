456174f4f1d3fba3c55f7b71b099baa437ec03bd
Mariariaardyanti <riaardyanti172gmail.com>
2026-01-14 18:54:23 +0700

    feat: add profile avatar image

--- lib/pages/home/profile_maria.dart
+++ lib/pages/home/profile_maria.dart
@@ -6,26 +6,29 @@ class ProfileMaria extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-            backgroundColor: Colors.white,
-            foregroundColor: Colors.black,
-            elevation: 0,
-            title: const Text(
-                'maria_euphrasia',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-            ),
-            body: SingleChildScrollView(
-                child: Column(
-                    children: [
-                        Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Container(),
-                            ),
-                    ],
-                ),
+        foregroundColor: Colors.black,
+        elevation: 0,
+        title: const Text(
+          'maria_euphrasia',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: const CircleAvatar(
+                radius: 45,
+                backgroundImage: AssetImage('assets/images/profile.jpg'),
               ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
