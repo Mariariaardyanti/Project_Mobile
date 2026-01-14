aac59a06de1c8aed47399ce541c06213ba0b7ab7
Mariariaardyanti <riaardyanti172gmail.com>
2026-01-14 18:45:45 +0700

    feat: add username title

--- lib/pages/home/profile_maria.dart
+++ lib/pages/home/profile_maria.dart
@@ -11,6 +11,10 @@ class ProfileMaria extends StatelessWidget {
             backgroundColor: Colors.white,
             foregroundColor: Colors.black,
             elevation: 0,
+            title: const Text(
+                'maria_euphrasia',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                ),
             ),
     );
   }
