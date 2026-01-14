9ed64883ac202c69e37b54347c4ddbaa02ad8090
Rian-61 <rian80344@gmail.com>
2026-01-14 20:40:39 +0700

    chore: mengimport note_model.dart ke workspace

--- lib/pages/workspace/workspace.dart
+++ lib/pages/workspace/workspace.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
+
+import 'package:project_mobile/models/note_model.dart';
