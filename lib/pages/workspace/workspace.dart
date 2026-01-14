ba8a9350b28c39481c1e2230f0f251e353863b57
Rian-61 <rian80344@gmail.com>
2026-01-14 20:47:50 +0700

    feat: add WorkspacePage stateful widget

--- lib/pages/workspace/workspace.dart
+++ lib/pages/workspace/workspace.dart
@@ -6,3 +6,11 @@ import 'package:intl/intl.dart';
 import 'package:project_mobile/models/note_model.dart';
 import 'package:project_mobile/services/notes_service.dart';
 import 'package:project_mobile/pages/notes/add_notes.dart';
+
+
+class WorkspacePage extends StatefulWidget {
+  const WorkspacePage({super.key});
+
+  @override
+  State<WorkspacePage> createState() => _WorkspacePageState();
+}
