d2339d070aacfc710dd5355fd9e24ef068301911
Rian-61 <rian80344@gmail.com>
2026-01-14 20:44:15 +0700

    chore: mengimport notes_service.dart ke workspace

--- lib/pages/workspace/workspace.dart
+++ lib/pages/workspace/workspace.dart
@@ -4,3 +4,4 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
 import 'package:project_mobile/models/note_model.dart';
+import 'package:project_mobile/services/notes_service.dart';
