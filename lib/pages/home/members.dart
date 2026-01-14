1ad272e4a1b4199e0470c06375ceb9cf92a41a08
AriPurwoAji <ajiaripurwo@gmail.com>
2026-01-14 21:30:11 +0700

    Menambahkan link ke halaman member_ari.dart

--- lib/pages/home/members.dart
+++ lib/pages/home/members.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:project_mobile/pages/notes/add_notes.dart';
 import 'package:project_mobile/pages/profile/profile.dart';
 
-
 class Members extends StatefulWidget {
   const Members({super.key});
 
@@ -156,7 +155,7 @@ class _MembersState extends State<Members> {
                         Icons.arrow_back_ios_new,
                         size: 20,
                         color: Colors.black,
-                      )
+                      ),
                     ),
 
                     const Text(
@@ -263,7 +262,14 @@ class _MembersState extends State<Members> {
                             Align(
                               alignment: Alignment.centerRight,
                               child: ElevatedButton(
-                                onPressed: () {},
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => Members(),
+                                    ),
+                                  );
+                                },
                                 style: ElevatedButton.styleFrom(
                                   padding: const EdgeInsets.symmetric(
                                     horizontal: 12,
