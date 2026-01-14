5dcbec829e8dd594461f8c7bbc1c3cf0ff5f2754
Mariariaardyanti <riaardyanti172gmail.com>
2026-01-14 18:41:44 +0700

    feat: add base profile page widget

--- lib/pages/home/profile_maria.dart
+++ lib/pages/home/profile_maria.dart
@@ -1,1 +1,10 @@
 import 'package:flutter/material.dart';
+
+class ProfileMaria extends StatelessWidget {
+  const ProfileMaria({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
