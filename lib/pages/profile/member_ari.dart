a61f7d485fbc3b1ef3948b96a8881e3d93fe91f9
AriPurwoAji <ajiaripurwo@gmail.com>
2026-01-14 17:13:58 +0700

    Memperbaiki ukuran text Role in project

--- lib/pages/profile/member_ari.dart
+++ lib/pages/profile/member_ari.dart
@@ -364,6 +364,7 @@ class _Member_AriState extends State<Member_Ari> {
                               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
                               style: TextStyle(
                                 fontSize: 12,
+                                height: 1.5,
                                 color: Colors.brown.shade700,
                               ),
                             ),
