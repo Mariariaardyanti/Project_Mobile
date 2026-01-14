aeeffd2c8602b201a6a863ef0a92335f734dd2e4
Rian-61 <rian80344@gmail.com>
2026-01-14 14:26:09 +0700

    chore: manambahkan import firebase massaging di main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'firebase_options.dart';
 import 'pages/home/homepage.dart';
