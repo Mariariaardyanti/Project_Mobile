7c53feb6ba8450366a561e6db7293e23319fcabe
Rian-61 <rian80344@gmail.com>
2026-01-15 18:28:14 +0700

    feat: initial setup for Rian profile page

--- lib/pages/home/profile_rian.dart
+++ lib/pages/home/profile_rian.dart
@@ -1,1 +1,18 @@
 import 'package:flutter/material.dart';
+
+class Rian extends StatefulWidget {
+  const Rian({super.key});
+
+  @override
+  State<Rian> createState() => _RianState();
+}
+
+class _RianState extends State<Rian> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF0F172A),
+      body: Container(), 
+    );
+  }
+}
