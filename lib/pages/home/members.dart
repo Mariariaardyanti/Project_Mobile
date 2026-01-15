b94c2362687733b7a2179aec38972289fe1e195b
Rian-61 <rian80344@gmail.com>
2026-01-15 20:00:44 +0700

    chore: menambahkan import profile_rian di members

--- lib/pages/home/members.dart
+++ lib/pages/home/members.dart
@@ -1,4 +1,5 @@
 import 'package:project_mobile/pages/home/profile_helen.dart';
+import 'package:project_mobile/pages/home/profile_rian.dart';
 import 'package:project_mobile/services/fcm_notification_service.dart';
 import 'package:project_mobile/pages/home/notification_page.dart';
 import 'package:flutter/material.dart';
