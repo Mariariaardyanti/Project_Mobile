f3d5b1c224d5702e67abe287c82152c242b4ba4e
AriPurwoAji <ajiaripurwo@gmail.com>
2026-01-16 20:15:39 +0700

    Menghapus Unused Import

--- lib/pages/home/profile_ari.dart
+++ lib/pages/home/profile_ari.dart
@@ -1,8 +1,4 @@
-import 'package:project_mobile/services/fcm_notification_service.dart';
-import 'package:project_mobile/pages/home/notification_page.dart';
 import 'package:flutter/material.dart';
-import 'package:project_mobile/pages/notes/add_notes.dart';
-import 'package:project_mobile/pages/profile/profile.dart';
 
 class Member_Ari extends StatefulWidget {
   const Member_Ari({super.key});
